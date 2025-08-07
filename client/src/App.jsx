96bfd3577811666aa121d24d7e5ddd531718e144
vedaant <vedaant.mahale@gmail.com>
2025-08-07 10:31:15 +0530

    login page dev started

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,18 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
+import Login from './login';
 
 function App() {
-
   return (
-    <>
-      <div className='bg-blue-300 w-screen h-screen'>
-        
-      </div>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App
---
+++ client/src/login.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function Login()
+{
+    return(
+
+        <>
+            <div className='bg-white w-screen h-screen flex-col flex justify-center items-center overflow-auto'>
+                <div className='w-1/2 h-20 rounded-2xl m-auto mt-10 flex justify-center items-center'>
+                <div className="flex text-7xl">
+                    <div className='text-blue-600'>Tiny</div>
+                    <div>Polls</div>
+                </div>
+                </div>
+                
+            </div>
+        </>
+    )
+}
+
+export default Login
