4f7e57258dee3af53eef0a1412d81f0190d9bb83
vedaant <vedaant.mahale@gmail.com>
2025-08-09 15:51:26 +0530

    forgor login button :skull:

--- client/src/auth.jsx
+++ client/src/auth.jsx
@@ -66,7 +66,7 @@ function Auth()
                             <input className='m-5 b-2 border-2 p-2 rounded-full border-black w-3/4' placeholder='Password' type="text" onChange={(e) => {setLogPass(e.target.value)}}></input>
                         </div>
                         <div className='mt-20 w-full flex flex-col items-center'>
-                            <div className='w-1/2 cursor-pointer rounded-full bg-black h-10 text-white flex justify-center items-center p-2'>LOGIN</div>
+                            <div className='w-1/2 cursor-pointer rounded-full bg-black h-10 text-white flex justify-center items-center p-2' onClick={onLogin}>LOGIN</div>
                         </div>
                     </div>
                 </div>
