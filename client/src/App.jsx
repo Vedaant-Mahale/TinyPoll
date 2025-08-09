29d8794d296ed180d5998969f04ad01b66d5a239
vedaant <vedaant.mahale@gmail.com>
2025-08-09 23:49:12 +0530

    minor path fixing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,12 +4,14 @@ import viteLogo from '/vite.svg'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
 import Auth from './auth';
+import Dashboard from './dashboard';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Auth />} />
+        <Route path="/dash" element={<Dashboard />} />
       </Routes>
     </Router>
   );
