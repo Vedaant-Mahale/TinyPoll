e548b02ce0a9bcd42cbfcc90f9d97752b8e4790b
vedaant <vedaant.mahale@gmail.com>
2025-08-11 14:20:34 +0530

    tinpyolls is now up on the top

--- client/src/dashboard.jsx
+++ client/src/dashboard.jsx
@@ -8,7 +8,7 @@ function Dashboard()
     const location = useLocation();
     const { name, id } = location.state || {};
     return (
-        <div className='bg-white w-screen h-screen flex-col flex justify-center items-center overflow-auto'>
+        <div className='bg-white w-screen h-40 flex-col flex justify-center items-center overflow-auto'>
             <div className='w-1/2 h-20 mt-40 md:mt-10 rounded-2xl flex justify-center items-center'>
                 <div className="flex text-7xl">
                     <div className='text-blue-600'>Tiny</div>
