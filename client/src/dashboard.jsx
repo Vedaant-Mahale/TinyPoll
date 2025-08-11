a6d737f4dbc9ab2b8f0e96f118473e5583951057
vedaant <vedaant.mahale@gmail.com>
2025-08-11 15:00:06 +0530

    added minor UI stuff in dashboard

--- client/src/dashboard.jsx
+++ client/src/dashboard.jsx
@@ -9,11 +9,13 @@ function Dashboard()
     const { name, id } = location.state || {};
     return (
         <div className='bg-white w-screen h-40 flex-col flex justify-center items-center overflow-auto'>
-            <div className='w-1/2 h-20 mt-40 md:mt-10 rounded-2xl flex justify-center items-center'>
+            <div className='w-full h-20 mt-40 md:mt-10 rounded-2xl flex justify-between items-center'>
+                <div className='w-30 h-30 rounded-full border-4 border-blue-700 flex justify-center items-center font-bold ml-20 text-blue-700'>PROFILE</div>
                 <div className="flex text-7xl">
                     <div className='text-blue-600'>Tiny</div>
                     <div>Polls</div>
                 </div>
+                <div className='w-30 h-30 rounded-full border-4 border-black flex justify-center items-center font-bold mr-20'>SET POLL</div>
             </div>
         </div>
     );
